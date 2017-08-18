ef523e2b1063688aef7dd8812e60d4d46b2682bb
FaalVedtuz <jmr_mymsaccnt@outlook.com>
2017-08-19 05:26:20 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+/* materialize imports */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdInputModule, MdButtonModule } from '@angular/material';
+/* ---------------------------*/
+
+/* flex layout module imports */
+ import { FlexLayoutModule } from '@angular/flex-layout';
+/* ---------------------------*/
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from '././components/login/login.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+const navigationRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavigationComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      navigationRoutes,
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MdCardModule,
+    MdInputModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class PizzaPartyAppModule { }
